8ef7998344464ce034a642ecf5293d9a640780c3
chandrianin <tbma39@yandex.ru>
2026-03-16 01:24:04 +0200

    adв a palette of material 3 colors

---
+++ src/shared/theme/getTheme.js
@@ -0,0 +1,50 @@
+import {createTheme} from "@mui/material/styles";
+import materialTokens from "./material-theme.json";
+
+
+export const getTheme = (mode) => {
+    const scheme = materialTokens.schemes[mode];
+
+
+    return createTheme({
+        palette: {
+            mode,
+
+            primary: {
+                main: scheme.primary,
+                contrastText: scheme.onPrimary
+            },
+            primaryContainer: scheme.primaryContainer,
+            onPrimaryContainer: scheme.onPrimaryContainer,
+
+            secondary: {
+                main: scheme.secondary,
+                contrastText: scheme.onSecondary
+            },
+
+            error: {
+                main: scheme.error,
+                contrastText: scheme.onError
+            },
+
+            background: {
+                default: scheme.background,
+                paper: scheme.surface
+            },
+
+            text: {
+                primary: scheme.onSurface,
+                secondary: scheme.onSurfaceVariant
+            },
+
+            divider: scheme.outlineVariant,
+            surface: scheme.surface,
+            surfaceContainer: scheme.surfaceContainer,
+            surfaceContainerLow: scheme.surfaceContainerLow,
+            surfaceContainerHigh: scheme.surfaceContainerHigh,
+            surfaceContainerHighest: scheme.surfaceContainerHighest
+        },
+
+
+    });
+}
