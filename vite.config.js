4d4520ea0b06b678957679f7f46f8d037f1c5996
chandrianin <tbma39@yandex.ru>
2026-03-21 19:34:25 +0200

    add manual division by chunks

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: "/",
-})
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('@mui')) return 'mui';
+                        if (id.includes('framer-motion')) return 'framer-motion';
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+    },
+});
