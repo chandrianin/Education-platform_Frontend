7571cc68f88f7edf221a1fcf8c5399abdfdf1842
Malysh Anastasiya <malishanastasiya@yandex.ru>
2026-05-01 16:16:21 +0200

    add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Цифровой помощник',
+        short_name: 'DA',
+        description: 'Приложение, помогающее учителям.',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'public/favicon_192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'public/favicon_512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })],
     build: {
         rollupOptions: {
             output: {
